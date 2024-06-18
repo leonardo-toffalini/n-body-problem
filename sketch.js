5a8b3ac96f0bd20175aff23d2127ea1d0459f41f
leonardo-toffalini <leonardotoffalini@gmail.com>
2024-06-18 16:05:06 +0200

    some refactoring

--- sketch.js
+++ sketch.js
@@ -1,59 +1,98 @@
-let planets = [];
-let numberOfPlanets = 5;
-let showSun = true;
-
 function setup() {
-  createCanvas(windowWidth, windowHeight - 40);
+    createCanvas(windowWidth, windowHeight - 40);
 
-  initializePlanets();
+    initializePlanets();
 
-  var button = createButton("Restart");
-  button.mousePressed(initializePlanets);
+    var button = createButton("Restart");
+    button.mousePressed(initializePlanets);
+}
+
+class Planet {
+  constructor(pos = null, v = null, a = null) {
+    if (pos == null) {
+      this.pos = createVector(random(width / 3, (2 * width) / 3), random(height / 3, (2 * height) / 3));
+    } else {
+      this.pos = pos;
+    }
+    if (v == null) {
+      this.v = createVector(random(-1, 1), random(-1, 1));
+    } else {
+      this.v = v;
+    }
+    if (a == null) {
+      this.a = createVector();
+    } else {
+      this.a = a;
+    }
+  }
+
+  checkBoundaries() {
+      if (this.pos.x - 20 <= 0 || this.pos.x + 20 >= width) {
+          this.v.x *= -1;
+      }
+      if (this.pos.y - 20 <= 0 || this.pos.y + 20 >= height) {
+          this.v.y *= -1;
+      }
+  }
 }
 
 function initializePlanets() {
-  for (let i = 0; i < numberOfPlanets; i++) {
-      let pos = p5.Vector.random2D();
-      let v = pos.copy();
-      v.setMag(random(3, 5));
-      pos.setMag(random(100, 150));
-      v.rotate(PI / 2);
-      let m = random(10, 15);
-      planets[i] = new Planet(pos.x, pos.y, v.x, v.y, m, this.color = [255, 153, 61, 255]);
-  }
-sun = new Planet(0, 0, 0, 0, 50, [255, 40, 55, 40]);
+  planet1 = new Planet();
+  planet2 = new Planet();
+  planet3 = new Planet();
+  planets = [planet1, planet2, planet3];
 }
 
-function checkBoundaries() {
-  for (planet of planets) {
-    if (planet.pos.x - 20 <= -width/2 || planet.pos.x + 20 >= width/2) {
-      planet.v.x *= -1;
+function updateAcceleration(planet) {
+  for (other of planets) {
+    if (planet == other) {
+      continue;
     }
-    if (planet.pos.y - 20 <= -height/2 || planet.pos.y + 20 >= height/2) {
-      planet.v.y *= -1;
-    }
+    tmp = p5.Vector.sub(other.pos, planet.pos);
+    tmp.div(tmp.mag());
+    planet.a = planet.a.add(tmp);
   }
 }
 
 function draw() {
   background(242, 238, 203, 50);
-  translate(width/2, height/2);
 
-  for (let planet of planets) {
-    sun.attract(planet);
-    for (let other of planets) {
-      if (other != planet) {
-        planet.attract(other);
-      }
-    }
+  for (planet of planets) {
+    updateAcceleration(planet);
   }
 
-  for (let planet of planets) {
-    planet.update();
-    planet.show();
+  for (planet of planets) {
+    planet.a.setMag(0.02);
+    planet.v = planet.v.add(planet.a);
+    planet.pos = planet.pos.add(planet.v);
   }
-  if (showSun) {
-    sun.show();
-    checkBoundaries();
+
+  // Representing attraction towards the center
+  // strokeWeight(2)
+  // for (planet of planets) {
+  //   line(planet.pos.x,
+  //     planet.pos.y,
+  //     (planets[0].pos.x + planets[1].pos.x + planets[2].pos.x) / 3,
+  //     (planets[0].pos.y + planets[1].pos.y + planets[2].pos.y) / 3);
+  // }
+
+  // Visualizing the planets
+
+  // noStroke();
+  stroke(255, 153, 61);
+  strokeWeight(4);
+  fill(255, 153, 61);
+  for (planet of planets) {
+    circle(planet.pos.x, planet.pos.y, 20);
+  }
+
+  stroke(255, 0, 0);
+
+  // Center of mass of the three planets
+  point((planet1.pos.x + planet2.pos.x + planet3.pos.x) / 3, (planet1.pos.y + planet2.pos.y + planet3.pos.y) / 3);
+
+  // Collision detection on the boundaries
+  for (planet of planets) {
+    planet.checkBoundaries();
   }
 }
